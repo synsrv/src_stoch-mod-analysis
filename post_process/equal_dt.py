6848bdbcee81c61ff38128168cdbf69a61928b46
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2019-12-02 09:42:52 +0100

    implemented log subsampling
    there is still an "issue" where logbins
    do not appear of equal size in the log plot,
    this is likely because of the substraction
    in the logbins.

--- post_process/equal_dt.py
+++ post_process/equal_dt.py
@@ -2,32 +2,53 @@
 import os, pickle
 import numpy as np
 
-from .lts_subsampling import subsamp_lts
+from .lts_subsampling import subsamp_lts, log_subsamp_lts
+from .true_lts_subsmp_pop import get_true_lts
 
 
 def subsamp_equal_dt(nsp, lts, dpath):
 
-    df = []
+    df, df_nw = [], []
+    
 
     # for k in [4, 5, 7, 8, 10, 12, 15, 17, 20, 22, 25, 27, 30, 32, 100,
     #           250, 500, 1000]:
-    for k in [5, 10, 25, 100, 2500]:
-
+    for k in [5, 10, 25, 50, 100, 250, 2500]:
+        
         dts, synsrv_prb, lts_subsmp = subsamp_lts(lts, nsp, k, start=0)
+
+        log_dts, log_synsrv_prb, log_lts_subsmp = log_subsamp_lts(lts,
+                                                                  nsp, k,
+                                                                  start=0)
 
         df_entry = {'k' : k, 'dts' : dts,
                     'synsrv_prb' : synsrv_prb,
                     'lts' : lts_subsmp,
+                    'log_dts': log_dts,
+                    'log_synsrv_prb': log_synsrv_prb,
+                    'log_lts_subsmp': log_lts_subsmp,
                     'start' : 0,
                     'dpath': dpath}
 
         df.append(df_entry)
 
         
-    fpath = 'data/' + dpath[-4:] + '/synsrv_prb_equal.p'
+        df_newins = get_true_lts(lts, nsp, k, start=0)
+        
+        df_entry = {'k' : k, 'dts' : dts,
+                    'start' : 0,
+                    'df_newins' : df_newins}
 
+        df_nw.append(df_entry)
+
+        
+    fpath = 'data/' + dpath[-4:] + '/synsrv_prb_equal.p'
     with open(fpath, 'wb') as pfile:
         pickle.dump(df, pfile)
+
+    fpath = 'data/' + dpath[-4:] + '/true_lts_equal.p'
+    with open(fpath, 'wb') as pfile:
+        pickle.dump(df_nw, pfile)
 
 
 
--- post_process/lts_subsampling.py
+++ post_process/lts_subsampling.py
@@ -34,7 +34,7 @@ def subsamp_lts(lts, nsp, n_samples, start=0):
         alive_synapse_ids = new_synapse_ids
         n_alive = len(alive_synapse_ids)
 
-        dts, subsmp_srvprb, subsmp_lts = [0.], [1.0], []
+        dts, subsmp_srvprb, subsmp_lts = [1.], [1.0], []
 
         for j in range(3, n_samples+1):
 
@@ -54,7 +54,7 @@ def subsamp_lts(lts, nsp, n_samples, start=0):
                 srv_prb = len(alive_synapse_ids)/len(new_synapse_ids)
 
                 
-            dts.append(j*dt-1)
+            dts.append((j-2)*dt+1)
             subsmp_srvprb.append(srv_prb)
 
         return dts, subsmp_srvprb, subsmp_lts
@@ -99,6 +99,71 @@ def subsamp_lts(lts, nsp, n_samples, start=0):
 
 
         # return dts, subsmp_srvprb
+    
+
+    
+
+
+
+def log_subsamp_lts(lts, nsp, n_samples, start=0):
+    ''' 
+    start=0 ---> equal dt method
+    start>0 ---> fixed start method, with start_dt=Nsteps/start
+    '''
+
+    df = pd.DataFrame(data=lts,
+                      columns=['empty', 'c', 't_elim', 't_ins',
+                               'elim_during_sim', 'weight', 'pid'])
+
+    df = df.astype({'c': 'int64', 't_elim': 'int64',
+                    't_ins': 'int64', 'elim_during_sim': 'int64',
+                    'pid': 'int64'})
+
+    if start==0:
+
+        dt = int(nsp['Nsteps']/(n_samples))
+
+        active_at_dt = df[(df['t_ins']< dt-1) &
+                          (df['t_elim'] >= dt-1)]['pid']
+        active_at_2dt = df[(df['t_ins']< 2*dt-1) &
+                           (df['t_elim'] >= 2*dt-1)]['pid']
+
+        new_synapse_ids = np.setdiff1d(active_at_2dt, active_at_dt)
+        alive_synapse_ids = new_synapse_ids
+        n_alive = len(alive_synapse_ids)
+
+        subsmp_srvprb, subsmp_lts = [1.0], []
+
+        dts = np.logspace(start=np.log10(2*dt),
+                          stop=np.log10(nsp['Nsteps']),
+                          num=n_samples-1)
+        
+        for j in range(len(dts)-1):
+
+            alive_until_j = df[(df['t_ins'] < dts[j]) &
+                               (df['t_elim'] >= dts[j]) &
+                               (df['pid'].isin(alive_synapse_ids))]
+
+            subsmp_lts.extend((n_alive - len(alive_until_j)) * [np.round(dts[j+1]-2*dt, 3)])
+            
+
+            alive_synapse_ids = alive_until_j['pid']
+            n_alive = len(alive_synapse_ids)
+
+            if len(new_synapse_ids) == 0:
+                srv_prb=1.0
+            else:
+                srv_prb = len(alive_synapse_ids)/len(new_synapse_ids)
+       
+            subsmp_srvprb.append(srv_prb)
+
+        return np.round(dts-2*dt,3), subsmp_srvprb, subsmp_lts
+    
+
+    else:
+
+        raise NotYetImplementedError
+
     
 
     
